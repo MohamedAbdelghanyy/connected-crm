b6bc9256af6df659c986d1cbedec1729415d0c02
MohamedAbdelghanyy <45740410+MohamedAbdelghanyy@users.noreply.github.com>
2023-08-11 19:26:07 +0300

    Update Dash Sample

--- src/app/dash/page.tsx
+++ src/app/dash/page.tsx
@@ -21,7 +21,6 @@ import { CalendarDateRangePicker } from "../../components/date-range-picker"
 import { MainNav } from "../../components/main-nav"
 import { Overview } from "../../components/overview"
 import { RecentSales } from "../../components/recent-sales"
-import { Search } from "../../components/search"
 import TeamSwitcher from "../../components/team-switcher"
 import { UserNav } from "../../components/user-nav"
 
@@ -35,9 +34,8 @@ export default function Dashboard() {
         <div className="border-b">
           <div className="flex h-16 items-center px-4">
             <TeamSwitcher />
-            <MainNav className="mx-6" />
+            <MainNav />
             <div className="ml-auto flex items-center space-x-4">
-              <Search />
               <UserNav />
             </div>
           </div>
