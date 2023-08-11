bd38eb4802cdc151cdd2ce7c1007aee2ac6d49ea
MohamedAbdelghanyy <45740410+MohamedAbdelghanyy@users.noreply.github.com>
2023-08-11 15:45:51 +0300

    Fix GH Actions

--- next.config.js
+++ next.config.js
@@ -1,24 +1,4 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  repoURL = "https://mohamedabdelghanyy.github.io/connected-crm/";
-  const repo = repoURL.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
-module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  output: 'export',
-  images: {
-    loader: 'imgix',
-    path: 'the "domain" of your Imigix source',
-  },
-}
+module.exports = nextConfig
