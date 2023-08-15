34009d017fed45f7212bfc26a08fd86f25d68800
MohamedAbdelghanyy <45740410+MohamedAbdelghanyy@users.noreply.github.com>
2023-08-15 16:05:56 +0300

    Update layout.tsx

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -5,6 +5,9 @@ import { MainNav } from "@/components/main-nav"
 import { DashboardNav } from "@/components/nav"
 import { SiteFooter } from "@/components/site-footer"
 import { UserAccountNav } from "@/components/user-account-nav"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { DocsSidebarNav } from "@/components/sidebar-nav"
+import { docsConfig } from "@/config/docs"
 
 interface DashboardLayoutProps {
   children?: React.ReactNode
@@ -15,26 +18,30 @@ export default async function DashboardLayout({
 }: DashboardLayoutProps) {
  
   return (
-    <div className="flex min-h-screen flex-col space-y-6">
-      <header className="sticky top-0 z-40 border-b bg-background">
-        <div className="container flex h-16 items-center justify-between py-4">
-          <MainNav items={dashboardConfig.mainNav} sideItems={dashboardConfig.sidebarNav} />
-          <UserAccountNav
-            user={{
-              name: "Mohamed Abdelghany",
-              image: "",
-              email: "mohamedabdelghanyy@gmail.com",
-            }}
-          />
+    <div className="border-b">
+      <div className="flex min-h-screen flex-col">
+        <header className="sticky top-0 z-40 border-b bg-background">
+          <div className="container flex h-16 items-center justify-between py-4">
+            <MainNav items={dashboardConfig.mainNav} sideItems={dashboardConfig.sidebarNav} />
+            <UserAccountNav
+              user={{
+                name: "Mohamed Abdelghany",
+                image: "",
+                email: "mohamedabdelghanyy@gmail.com",
+              }}
+            />
+          </div>
+        </header>
+        <div className="container flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
+          <aside className="fixed top-14 z-30 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 md:sticky md:block">
+            <ScrollArea className="h-full py-6 pr-6">
+              <DashboardNav items={dashboardConfig.sidebarNav} />
+            </ScrollArea>
+          </aside>
+          <main className="relative py-6">
+            {children}
+          </main>
         </div>
-      </header>
-      <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
-        <aside className="hidden w-[200px] flex-col md:flex">
-          <DashboardNav items={dashboardConfig.sidebarNav} />
-        </aside>
-        <main className="flex w-full flex-1 flex-col overflow-hidden">
-          {children}
-        </main>
       </div>
       <SiteFooter className="border-t" />
     </div>
