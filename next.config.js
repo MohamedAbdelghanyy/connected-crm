fb313b4b8dd70ab5b251b3fba5376acd4c83a96b
Mohamed Abdelghany <mohamedabdelghanyy@gmail.com>
2023-10-09 15:28:49 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    //output: 'export',
+    //output: 'export', // Needed for GitHub Actions but it disables dynamic routes
 }
 
 module.exports = nextConfig
