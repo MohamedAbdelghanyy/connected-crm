7219339311998594dff5bbe3ea5ea6d0d33e0bad
Mohamed Abdelghany <mohamedabdelghanyy@gmail.com>
2023-10-16 17:59:22 +0300

    CEO Dashboard

--- src/app/(home)/ceo-dashboard/components/charts/dashboard-charts.tsx
+++ src/app/(home)/ceo-dashboard/components/charts/dashboard-charts.tsx
@@ -1,23 +1,27 @@
 "use client"
 
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { Grid } from '@mui/material';
 import {
+  BarElement,
   CategoryScale,
   Chart as ChartJS,
   Filler,
   Legend,
-  LineElement,
   LinearScale,
   PointElement,
   Title,
   Tooltip,
 } from 'chart.js';
-import { Line } from 'react-chartjs-2';
+import { useEffect, useState } from 'react';
+import { Bar } from 'react-chartjs-2';
 
 ChartJS.register(
   CategoryScale,
   LinearScale,
   PointElement,
-  LineElement,
+  BarElement,
   Title,
   Tooltip,
   Filler,
@@ -46,7 +50,7 @@ export const options = {
           let visibility: any = [];
           for (let i = 0; i < chart.data.datasets.length; i++) {
             if (chart.isDatasetVisible(i) === true) {
-              visibility.push('rgba(255, 255, 255, 0.8)');
+              visibility.push('rgba(255, 255, 255, 1)');
             } else {
               visibility.push('rgba(255, 255, 255, 0.3)');
             }
@@ -69,70 +73,130 @@ export const options = {
   },
 };
 
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
-export const data = {
-  labels,
-  datasets: [
-    {
-      fill: true,
-      label: 'New Installs',
-      data: labels.map(() => Math.floor(Math.random() * 900)),
-      borderColor: 'rgb(0, 255, 0)',
-      backgroundColor: 'rgba(0, 255, 0, 0.25)',
-      hidden: true,
-    },
-    {
-      fill: true,
-      label: 'New Un-installs',
-      data: labels.map(() => Math.floor(Math.random() * 900)),
-      borderColor: 'rgb(255, 0, 0)',
-      backgroundColor: 'rgba(255, 0, 0, 0.25)',
-      hidden: true,
-    },
-    {
-      fill: true,
-      label: 'New Users',
-      data: labels.map(() => Math.floor(Math.random() * 900)),
-      borderColor: 'rgb(255, 255, 0)',
-      backgroundColor: 'rgba(255, 255, 0, 0.25)',
-      hidden: true,
-    },
-    {
-      fill: true,
-      label: 'New Items',
-      data: labels.map(() => Math.floor(Math.random() * 900)),
-      borderColor: 'rgb(53, 162, 235)',
-      backgroundColor: 'rgba(53, 162, 235, 0.25)',
-      hidden: true,
-    },
-    {
-      fill: true,
-      label: 'Sold Items',
-      data: labels.map(() => Math.floor(Math.random() * 900)),
-      borderColor: 'rgb(255, 165, 0)',
-      backgroundColor: 'rgba(255, 165, 0, 0.25)',
-      hidden: true,
-    },
-    {
-      fill: true,
-      label: 'Active Users',
-      data: labels.map(() => Math.floor(Math.random() * 900)),
-      borderColor: 'rgb(255, 0, 255)',
-      backgroundColor: 'rgba(255, 0, 255, 0.25)',
-      hidden: true,
-    },
-    {
-      fill: true,
-      label: 'New Requests',
-      data: labels.map(() => Math.floor(Math.random() * 900)),
-      borderColor: 'rgb(128, 0, 128)',
-      backgroundColor: 'rgba(128, 0, 128, 0.25)',
-      hidden: true,
-    },
-  ],
-};
-
 export function DashboardCharts() {
-  return <Line options={options} data={data} />;
+  const [chartLabels, setChartLabels] = useState([]);
+  const [chartData, setChartData] = useState({
+    labels: [''],
+    datasets: [
+      {
+        fill: true,
+        label: '',
+        data: chartLabels.map(() => Math.floor(Math.random() * 900)),
+        borderColor: '',
+        backgroundColor: '',
+        hidden: true,
+      }
+    ]
+  });
+
+  useEffect(() => {
+    changeLabels('month');
+  }, []);
+
+  useEffect(() => {
+    setChartData({
+      labels: chartLabels,
+      datasets: [
+        {
+          fill: true,
+          label: 'New Installs',
+          data: chartLabels.map(() => Math.floor(Math.random() * 900)),
+          borderColor: 'rgb(0, 255, 0)',
+          backgroundColor: 'rgba(0, 255, 0, 0.6)',
+          hidden: true,
+        },
+        {
+          fill: true,
+          label: 'New Un-installs',
+          data: chartLabels.map(() => Math.floor(Math.random() * 900)),
+          borderColor: 'rgb(255, 0, 0)',
+          backgroundColor: 'rgba(255, 0, 0, 0.6)',
+          hidden: true,
+        },
+        {
+          fill: true,
+          label: 'New Users',
+          data: chartLabels.map(() => Math.floor(Math.random() * 900)),
+          borderColor: 'rgb(255, 255, 0)',
+          backgroundColor: 'rgba(255, 255, 0, 0.6)',
+          hidden: true,
+        },
+        {
+          fill: true,
+          label: 'New Items',
+          data: chartLabels.map(() => Math.floor(Math.random() * 900)),
+          borderColor: 'rgb(53, 162, 235)',
+          backgroundColor: 'rgba(53, 162, 235, 0.6)',
+          hidden: true,
+        },
+        {
+          fill: true,
+          label: 'Sold Items',
+          data: chartLabels.map(() => Math.floor(Math.random() * 900)),
+          borderColor: 'rgb(255, 165, 0)',
+          backgroundColor: 'rgba(255, 165, 0, 0.6)',
+          hidden: true,
+        },
+        {
+          fill: true,
+          label: 'Active Users',
+          data: chartLabels.map(() => Math.floor(Math.random() * 900)),
+          borderColor: 'rgb(255, 0, 255)',
+          backgroundColor: 'rgba(255, 0, 255, 0.6)',
+          hidden: true,
+        },
+        {
+          fill: true,
+          label: 'New Requests',
+          data: chartLabels.map(() => Math.floor(Math.random() * 900)),
+          borderColor: 'rgb(128, 0, 128)',
+          backgroundColor: 'rgba(128, 0, 128, 0.6)',
+          hidden: true,
+        },
+      ],
+    });
+  }, [chartLabels]);
+
+  const changeLabels = (type: string) => {
+    let newLabels: any = [];
+    if (type == 'week') {
+      newLabels = ['Week 1', 'Week 2', 'Week 3', 'Week 4'];
+    } else if (type == 'month') {
+      newLabels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    } else if (type == 'quarter') {
+      newLabels = ['Q1', 'Q2', 'Q3', 'Q4'];
+    } else if (type == 'year') {
+      newLabels = ['2021', '2022', '2023']
+    }
+    setChartLabels(newLabels);
+  }
+  return (
+    <Card className="col-span-4 mb-4">
+      <CardHeader>
+        <Grid container spacing={2}>
+          <Grid item lg={6} xs={6}>
+            <CardTitle>Overview</CardTitle>
+          </Grid>
+          <Grid item lg={6} xs={6}>
+            <div style={{ float: "right" }}>
+              <Select onValueChange={changeLabels}>
+                <SelectTrigger className="w-[160px]">
+                  <SelectValue placeholder="Month" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="week">Week</SelectItem>
+                  <SelectItem value="month">Month</SelectItem>
+                  <SelectItem value="quarter">Quarter</SelectItem>
+                  <SelectItem value="year">Year</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+          </Grid>
+        </Grid>
+      </CardHeader>
+      <CardContent className="pl-2">
+        <Bar options={options} data={chartData} />
+      </CardContent>
+    </Card>
+  );
 }
--- src/app/(home)/ceo-dashboard/page.tsx
+++ src/app/(home)/ceo-dashboard/page.tsx
@@ -4,10 +4,6 @@ import {
   CardHeader,
   CardTitle
 } from "@/components/ui/card"
-import {
-  Tabs,
-  TabsContent
-} from "@/components/ui/tabs"
 import Grid from '@mui/material/Grid'
 import { DashboardCharts } from "./components/charts/dashboard-charts"
 import { Top10ContactOwner } from "./components/top-10/top-10-contact-owner"
@@ -25,160 +21,128 @@ export default function CEODashboardPage() {
       <div className="flex items-center justify-between space-y-2">
         <h2 className="text-4xl mb-1">Dashboard</h2>
       </div>
-      <Tabs defaultValue="overview" className="space-y-4 mt-2">
-        {/*<Grid container spacing={1}>
-          <Grid item xs={12} sm={12} md={5} lg={4}>
-            <TabsList style={{ width: "100%", justifyContent: "space-between" }}>
-              <TabsTrigger value="overview">Overview</TabsTrigger>
-              <TabsTrigger value="analytics" disabled>
-                Analytics
-              </TabsTrigger>
-              <TabsTrigger value="reports" disabled>
-                Reports
-              </TabsTrigger>
-            </TabsList>
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4 mt-2">
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">
+              Total Installs
+            </CardTitle>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="h-4 w-4 text-muted-foreground"
+            >
+              <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+              <circle cx="9" cy="7" r="4" />
+              <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
+            </svg>
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">12529</div>
+            <p className="text-xs text-muted-foreground">
+              +1871 from last month
+            </p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">Conversion Rate</CardTitle>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="h-4 w-4 text-muted-foreground"
+            >
+              <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
+            </svg>
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">40%</div>
+            <p className="text-xs text-muted-foreground">
+              +8% from last month
+            </p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">
+              Active Users
+            </CardTitle>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="h-4 w-4 text-muted-foreground"
+            >
+              <rect width="20" height="14" x="2" y="5" rx="2" />
+              <path d="M2 10h20" />
+            </svg>
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">5273</div>
+            <p className="text-xs text-muted-foreground">
+              +106 since yesterday
+            </p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">
+              New Users
+            </CardTitle>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="h-4 w-4 text-muted-foreground"
+            >
+              <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+              <circle cx="9" cy="7" r="4" />
+              <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
+            </svg>
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">2350</div>
+            <p className="text-xs text-muted-foreground">
+              +230 from last month
+            </p>
+          </CardContent>
+        </Card>
+      </div>
+      <div className="mt-4">
+        <DashboardCharts />
+        <Grid container spacing={2}>
+          <Grid item lg={6} xs={12}>
+            <Top10Viewed />
           </Grid>
-          <Grid item xs={0} sm={0} md={2} lg={3}></Grid>
-          <Grid item xs={12} sm={12} md={5} lg={5}>
-            <div className="flex items-center space-x-2" style={{ float: "right" }}>
-              <CalendarDateRangePicker />
-              <Button>Download</Button>
-            </div>
+          <Grid item lg={6} xs={12}>
+            <Top10Shared />
           </Grid>
-        </Grid>*/}
-        <TabsContent value="overview" className="space-y-4">
-          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-            <Card>
-              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-sm font-medium">
-                  Total Installs
-                </CardTitle>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="h-4 w-4 text-muted-foreground"
-                >
-                  <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-                  <circle cx="9" cy="7" r="4" />
-                  <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
-                </svg>
-              </CardHeader>
-              <CardContent>
-                <div className="text-2xl font-bold">12529</div>
-                <p className="text-xs text-muted-foreground">
-                  +1871 from last month
-                </p>
-              </CardContent>
-            </Card>
-            <Card>
-              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-sm font-medium">Conversion Rate</CardTitle>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="h-4 w-4 text-muted-foreground"
-                >
-                  <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-                </svg>
-              </CardHeader>
-              <CardContent>
-                <div className="text-2xl font-bold">40%</div>
-                <p className="text-xs text-muted-foreground">
-                  +8% from last month
-                </p>
-              </CardContent>
-            </Card>
-            <Card>
-              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-sm font-medium">
-                  Active Users
-                </CardTitle>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="h-4 w-4 text-muted-foreground"
-                >
-                  <rect width="20" height="14" x="2" y="5" rx="2" />
-                  <path d="M2 10h20" />
-                </svg>
-              </CardHeader>
-              <CardContent>
-                <div className="text-2xl font-bold">5273</div>
-                <p className="text-xs text-muted-foreground">
-                  +106 since yesterday
-                </p>
-              </CardContent>
-            </Card>
-            <Card>
-              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-sm font-medium">
-                  Total Un-installs
-                </CardTitle>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="h-4 w-4 text-muted-foreground"
-                >
-                  <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-                  <circle cx="9" cy="7" r="4" />
-                  <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
-                </svg>
-              </CardHeader>
-              <CardContent>
-                <div className="text-2xl font-bold">2350</div>
-                <p className="text-xs text-muted-foreground">
-                  +230 from last month
-                </p>
-              </CardContent>
-            </Card>
-          </div>
-          <div //className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
-          >
-            <Card className="col-span-4 mb-4">
-              <CardHeader>
-                <CardTitle>Overview</CardTitle>
-              </CardHeader>
-              <CardContent className="pl-2">
-                <DashboardCharts />
-              </CardContent>
-            </Card>
-            <Grid container spacing={2}>
-              <Grid item lg={6} xs={12}>
-                <Top10Viewed />
-              </Grid>
-              <Grid item lg={6} xs={12}>
-                <Top10Shared />
-              </Grid>
-              <Grid item lg={6} xs={12}>
-                <Top10Searched />
-              </Grid>
-              <Grid item lg={6} xs={12}>
-                <Top10ContactOwner />
-              </Grid>
-            </Grid>
-          </div>
-        </TabsContent>
-      </Tabs>
+          <Grid item lg={6} xs={12}>
+            <Top10Searched />
+          </Grid>
+          <Grid item lg={6} xs={12}>
+            <Top10ContactOwner />
+          </Grid>
+        </Grid>
+      </div>
     </>
   )
 }
